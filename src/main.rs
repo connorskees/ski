b25e26c0287b3e94b8fd863e0736316e2d32709c
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2019-10-13 20:47:10 -0400

    Read from file rather than stdin

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 #![deny(missing_debug_implementations)]
 use std::io::{self, stdin};
+use std::io::prelude::*;
+use std::fs::File;
 use lexer::Lexer;
 
 mod lexer;
-    // let input = if let Ok(ss) = String::from_utf8(input_) { ss } else { String::new() };
 
 fn main() -> io::Result<()> {
+    let mut f = File::open("test.ski")?;
     let mut input = String::new();
     let mut lexer = Lexer::new();
-    stdin().read_line(&mut input)?;
+    // stdin().read_line(&mut input)?;
+    f.read_to_string(&mut input)?;
 
     let tokens = lexer.lex(&input);
 

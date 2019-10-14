bcb3a4ebd026888d2bd2a9004016b1746d75e3a9
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2019-10-13 20:12:38 -0400

    Refactor lexer into separate file

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,2 +1,336 @@
-use std::io::{BufRead, stdin};
+use regex;
 
+#[derive(Debug, Hash, Eq, PartialEq)]
+pub enum Literal {
+    Str(String),
+    Int(u64),
+    Bool(bool),
+    None
+}
+
+#[derive(Debug, Hash, Eq, PartialEq)]
+pub enum Token {
+    Identifier(String),
+    Literal(Literal),
+    OpenBracket,
+    CloseBracket,
+    Function,
+    OpenParen,
+    CloseParen,
+    Let,
+    Const,
+    For,
+    While,
+    Loop,
+    SemiColon,
+    DoubleEqual,
+    Equal,
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Pow,
+    AddAssign,
+    SubAssign,
+    MulAssign,
+    DivAssign,
+    SingleQuote,
+    DoubleQuote,
+    Return,
+    Gt,
+    Lt,
+    GtEq,
+    LtEq,
+    Shr,
+    Shl,
+    Xor,
+    LogicalAnd,
+    LogicalOr,
+    BinaryAnd,
+    BinaryOr,
+    If,
+    Else
+}
+
+#[derive(Debug)]
+pub struct Lexer {
+    col: u16,
+    row: u16
+}
+
+impl Token {
+    pub fn new(token: &str) -> Token {
+        match token {
+            "{" => Token::OpenBracket,
+            "}" => Token::CloseBracket,
+            "fn" => Token::Function,
+            "(" => Token::OpenParen,
+            ")" => Token::CloseParen,
+            "let" => Token::Let,
+            "const" => Token::Const,
+            "for" => Token::For,
+            "while" => Token::While,
+            "loop" => Token::Loop,
+            "return" => Token::Return,
+            "if" => Token::If,
+            "else" => Token::Else,
+            "=" => Token::Equal,
+            "==" => Token::DoubleEqual,
+            ";" => Token::SemiColon,
+            "'" => Token::SingleQuote,
+            "\"" => Token::DoubleQuote,
+            "+" => Token::Add,
+            "-" => Token::Sub,
+            "*" => Token::Mul,
+            "/" => Token::Div,
+            "**" => Token::Pow,
+            "+=" => Token::AddAssign,
+            "-=" => Token::SubAssign,
+            ">" => Token::Gt,
+            "<" => Token::Lt,
+            ">=" => Token::GtEq,
+            "<=" => Token::LtEq,
+            ">>" => Token::Shr,
+            "<<" => Token::Shl,
+            "^" => Token::Xor,
+            "&" => Token::BinaryAnd,
+            "|" => Token::BinaryOr,
+            "&&" => Token::LogicalAnd,
+            "||" => Token::LogicalOr,
+            "true" => Token::Literal(Literal::Bool(true)),
+            "false" => Token::Literal(Literal::Bool(false)),
+            _ => {
+                Token::Identifier(token.to_owned())
+            }
+        }
+    }
+}
+
+macro_rules! double_identifier {
+    ( $i:literal, $cur:ident, $idents:ident ) => {
+        if $cur == $i {
+            $idents.push(Token::new(concat!($i, $i)));
+            $cur = "";
+            continue;
+        }
+        
+        if $cur != "" {
+            $idents.push(Token::new($cur));
+        }
+
+        $cur = $i;
+    }
+}
+
+impl Lexer {
+    pub fn new() -> Lexer {
+        Lexer {
+            row: 0,
+            col: 0
+        }
+    }
+
+    pub fn strip_comments(input: String) -> Result<String, regex::Error> {
+        let single_line = regex::Regex::new(r"//[^\n\r]*");
+        unimplemented!()
+    }
+
+    pub fn lex(&mut self, input: String) -> Vec<Token> {
+        let mut tokens: Vec<Token> = Vec::with_capacity(40);
+
+        let mut ci: String;
+
+        let mut current_identifier: &str = "";
+
+        let mut literal = Literal::None;
+
+        let mut integer_base: u32 = 10;
+        let mut next_char_is_escaped = false;
+
+        for c in input.chars() {
+            self.col += 1;
+            match literal {
+                Literal::Str(_) => {
+                    if c == '"' {
+                        tokens.push(Token::Literal(Literal::Str(current_identifier.to_owned())));
+                        current_identifier = "";
+                        literal = Literal::None;
+                        continue;
+                    }
+
+                    ci = format!("{}{}", current_identifier, c);
+                    current_identifier = ci.as_ref();
+                    continue;
+                },
+                Literal::Int(_) => {
+                    match c {
+                        '0'..='9' => {
+                            ci = format!("{}{}", current_identifier, c);
+                            current_identifier = ci.as_ref();
+                            continue;
+                        },
+                        'a'..='f' | 'A'..='F' => {
+                            if integer_base == 16 {
+                                ci = format!("{}{}", current_identifier, c);
+                                current_identifier = ci.as_ref();
+                            } else {
+                                unimplemented!()
+                            }
+                            continue;
+                        }
+                        'x' => {
+                            if current_identifier.len() == 1 {
+                                current_identifier = "";
+                                integer_base = 16;
+                            } else {
+                                unimplemented!()
+                            }
+                            continue;
+                        },
+                        _ => {
+                            tokens.push(Token::Literal(Literal::Int(u64::from_str_radix(current_identifier, integer_base).unwrap())));
+                            integer_base = 10;
+                            current_identifier = "";
+                            literal = Literal::None;
+                        }
+                    } 
+                },
+                _ => {}
+            }
+
+            match c {
+                ' ' | '\r' | '\t' => {
+                    if current_identifier != "" {
+                        tokens.push(Token::new(current_identifier));
+                        current_identifier = "";
+                    }
+                },
+                '\n' => {
+                    if current_identifier != "" {
+                        tokens.push(Token::new(current_identifier));
+                        current_identifier = "";
+                    }
+                    self.row += 1;
+                    self.col = 0;
+                }
+                '{' | '}' | '(' | ')' | ';' | '^' => {
+                    if current_identifier != "" {
+                        tokens.push(Token::new(current_identifier));
+                        current_identifier = "";
+                    }
+                    tokens.push(Token::new(&c.to_string()));
+                },
+                '+' => {
+                    if current_identifier != "" {
+                        tokens.push(Token::new(current_identifier));
+                    }
+                    current_identifier = "+";
+                },
+                '-' => {
+                    if current_identifier != "" {
+                        tokens.push(Token::new(current_identifier));
+                    }
+                    current_identifier = "-";
+                },
+                '"' => {
+                    literal = Literal::Str(String::new());
+                    continue;
+                }
+                '=' => {
+                    if current_identifier == "=" {
+                        tokens.push(Token::DoubleEqual);
+                        current_identifier = "";
+                        continue;
+                    }
+
+                    if current_identifier == "+" {
+                        tokens.push(Token::AddAssign);
+                        current_identifier = "";
+                        continue;
+                    }
+
+                    if current_identifier == "-" {
+                        tokens.push(Token::SubAssign);
+                        current_identifier = "";
+                        continue;
+                    }
+
+                    if current_identifier == "*" {
+                        tokens.push(Token::MulAssign);
+                        current_identifier = "";
+                        continue;
+                    }
+
+                    if current_identifier == "/" {
+                        tokens.push(Token::DivAssign);
+                        current_identifier = "";
+                        continue;
+                    }
+
+                    if current_identifier == "<" {
+                        tokens.push(Token::LtEq);
+                        current_identifier = "";
+                        continue;
+                    }
+
+                    if current_identifier == ">" {
+                        tokens.push(Token::GtEq);
+                        current_identifier = "";
+                        continue;
+                    }
+
+
+                    
+                    if current_identifier != "" {
+                        tokens.push(Token::new(current_identifier));
+                    }
+
+                    current_identifier = "=";
+                }
+                '&' => {
+                    double_identifier!("&", current_identifier, tokens);
+                }
+                '|' => {
+                    double_identifier!("|", current_identifier, tokens);
+                }
+                '*' => {
+                    double_identifier!("*", current_identifier, tokens);
+                }
+                '/' => {
+                    double_identifier!("/", current_identifier, tokens);
+                }
+                '>' => {
+                    double_identifier!(">", current_identifier, tokens);
+                }
+                '<' => {
+                    double_identifier!("<", current_identifier, tokens);
+                }
+                '0'..='9' => {
+                    literal = Literal::Int(0);
+                    if current_identifier != "" {
+                        tokens.push(Token::new(current_identifier));
+                        current_identifier = "";
+                    }
+                    ci = format!("{}{}", current_identifier, c);
+                    current_identifier = ci.as_ref();
+            }
+                _ => {
+                    match current_identifier {
+                        "=" | "&" | "|" | "*" | "/" | "<" | ">" | "+" | "-" => {
+                            tokens.push(Token::new(current_identifier));
+                            current_identifier = "";
+                        }
+                        _ => {}
+                    }
+                    ci = format!("{}{}", current_identifier, c);
+                    current_identifier = ci.as_ref();
+                }
+            }
+        }
+
+        if current_identifier != "" {
+            tokens.push(Token::new(current_identifier));
+        }
+        tokens
+    }
+}

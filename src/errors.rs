18a2e5f0fa22ea57b3d161501df551e1c11fe560
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2019-10-20 13:22:07 -0400

    Restructure errors

--- src/errors.rs
+++ src/errors.rs
@@ -1,38 +1,26 @@
 use std::fmt;
 
-use regex::Error;
-
 /// Container for errors that can occur during lexing
 #[derive(Debug)]
 pub enum LexingError {
     InvalidIntegerLiteralCharacter,
     RegexError(regex::Error),
 }
+
+impl std::convert::From<regex::Error> for LexingError {
+    fn from(error: regex::Error) -> Self {
+        LexingError::RegexError(error)
+    }
+}
+
 #[derive(Debug)]
 pub enum ParseError {
     Eof,
     Error(&'static str)
 }
 
-impl fmt::Display for LexingError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        use LexingError::*;
-        match self {
-            _ => {
-                write!(f, "")
-            }
-        }
-    }
-}
-
 impl std::convert::From<&'static str> for ParseError {
     fn from(error: &'static str) -> Self {
         ParseError::Error(error)
-    }
-}
-
-impl std::convert::From<regex::Error> for LexingError {
-    fn from(error: regex::Error) -> Self {
-        LexingError::RegexError(error)
     }
 }
